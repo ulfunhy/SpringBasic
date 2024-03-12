bb31193b40765958e9de7e61db7eba32cba4612b
ulfunhy <myfamily11141@gmail.com>
2024-03-12 15:13:47 +0900

    03-12 :: TestBean2 생성 및 getBean() 활용, 옵션 추가

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -1,17 +1,24 @@
 package kr.hs.study;
 
+import kr.hs.study.beans.TestBean;
+import kr.hs.study.beans.TestBean2;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        TestBean obj1 = ctx.getBean("t1", TestBean.class);
+        System.out.println("obj1:"+obj1);
+
+        TestBean2 obj2 = ctx.getBean("t2", TestBean2.class);
+        System.out.println("obj2:" +obj2);
+
+        TestBean2 obj3 = ctx.getBean("t2", TestBean2.class);
+        System.out.println("obj3:" +obj3);
+
+        ctx.close();
     }
 }
